1d6ae9a1cb7e9b603da89ef48b40bcc0d1f75aa3
Root99 <azizrezgui4@gmail.com>
2023-05-10 23:35:55 +0100

    app context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { CartProvider } from '../context/Cart/CartContext';
+
 export default function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      {/* eslint-disable-next-line react/jsx-filename-extension,react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
