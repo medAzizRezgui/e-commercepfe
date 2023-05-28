46be2d1ba049460bb61d90d5ace146d6aacf0e83
Root99 <azizrezgui4@gmail.com>
2023-05-28 03:07:14 +0100

    fixing cors errors

--- src/pages/api/axios.ts
+++ src/pages/api/axios.ts
@@ -3,4 +3,12 @@ import axios from 'axios';
 const axiosProduction = axios.create({
   baseURL: 'https://lazy-gray-termite-cape.cyclic.app', // Set your API base URL here
 });
+
+axiosProduction.defaults.headers.common['Access-Control-Allow-Origin'] =
+  'http://localhost:3000'; // Replace with your frontend origin
+axiosProduction.defaults.headers.common['Access-Control-Allow-Methods'] =
+  'GET, POST, DELETE, UPDATE, PUT, PATCH';
+axiosProduction.defaults.headers.common['Access-Control-Allow-Credentials'] =
+  'true';
+
 export default axiosProduction;
