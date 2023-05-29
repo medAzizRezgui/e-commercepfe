92955ee1d52f79db5da2b7cb999380a3b52dfb78
Root99 <azizrezgui4@gmail.com>
2023-05-29 05:01:21 +0100

    removed stripe and implemented flouci

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -38,7 +38,7 @@ const Checkout = () => {
     const requestData = {
       app_token: '955ddff9-f0e3-4e8e-8b74-ab30e3b53edf',
       app_secret: 'da3f95a4-2fd5-4c8c-8c49-7cac799e2433',
-      amount: '350',
+      amount: parseInt(calculateTotal(), 10) * 1000,
       accept_card: true,
       session_timeout_secs: 1200,
       success_link: `https://www.rezgui-aziz.me/success?orderId=${orderId}`,
