789a20ae58cedc8d899d6b2b0e8accd71e5de7fa
Root99 <azizrezgui4@gmail.com>
2023-10-03 21:01:08 +0100

    build fixes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/https://pfeecommerce.azurewebsites.net/:path*',
+        source: 'https://pfeecommerce.azurewebsites.net/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
